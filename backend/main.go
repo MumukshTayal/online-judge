67d106c80f3149c6d0b40ac922240dc5c94b1dd1
Pushpendra Pratap Singh <pushpendrasingh.19012@gmail.com>
2024-02-17 17:35:59 +0530

    add GET request: get problem by user_id

--- backend/main.go
+++ backend/main.go
@@ -19,15 +19,28 @@ func main() {
 
 	app.Get("/google_login", controllers.GoogleLogin)
 	app.Get("/google_callback", controllers.GoogleCallback)
-	app.Get("/get_contest_users", get_contestUsers.GetContestUsers)
-	app.Get("/get_problem", func(c *fiber.Ctx) error {
+	app.Get("/get_all_contests_users_pairs", get_contestUsers.GetContestUsers)
+	app.Get("/get_problem_by_problemId", func(c *fiber.Ctx) error {
 		problemIDStr := c.Query("problem_id")
 		problemID, err := strconv.Atoi(problemIDStr)
 		if err != nil {
 			return err
 		}
 
-		return get_problem.GetProblem(c, problemID)
+		return get_problem.GetProblemByProblemId(c, problemID)
+	})
+	app.Get("/get_problem_by_userId", func(c *fiber.Ctx) error {
+		userIDStr := c.Query("user_id")
+		userId, err := strconv.Atoi(userIDStr)
+		if err != nil {
+			return err
+		}
+
+		problems, err := get_problem.GetProblemByUserId(c, userId)
+		if err != nil {
+			return err
+		}
+		return c.JSON(problems)
 	})
 	app.Post("/create_contest", add_contest.AddContest)
 	app.Post("/create_problem", add_problem.AddProblem)
